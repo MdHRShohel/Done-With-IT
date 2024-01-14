1a424eef26503a094d9d954c93f72dce7b6e6955
mdhrshohel <md.hrshohel@gmail.com>
2024-01-14 16:34:03 +0600

    push

---
+++ App.js
@@ -0,0 +1,10 @@
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+
+  const handlePress = () => console.log('Text pressed');
+
+  return (
+	<WelcomeScreen />
+  );
+}
